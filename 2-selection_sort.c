f6e34bd385f2c3f8a09bba4d565a5b235ddf868f
djso89 <danielso89@gmail.com>
2020-06-18 00:20:21 -0700

    added if

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -31,7 +31,10 @@ void selection_sort(int *array, size_t size)
 			if (array[j] < array[min])
 				min = j;
 		}
-		swap_int(&array[min], &array[i]);
-		print_array(array, size);
+		if (i != min)
+		{
+			swap_int(&array[min], &array[i]);
+			print_array(array, size);
+		}
 	}
 }
