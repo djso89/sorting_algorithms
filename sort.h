bca09b91197024bbea4cfbc5489951ecda0cf6cb
djso89 <danielso89@gmail.com>
2020-06-17 21:58:14 -0700

    insertion_sort_list

--- sort.h
+++ sort.h
@@ -21,4 +21,6 @@ void print_array(const int *array, size_t size);
 /*0-bubble_sort.c */
 void bubble_sort(int *array, size_t size);
 
+/*1-insertion_sort_list.c*/
+void insertion_sort_list(listint_t **list);
 #endif
