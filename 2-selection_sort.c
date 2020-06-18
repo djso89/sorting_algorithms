60126440b942dc94d0621c0c89e518e2fef4d02d
djso89 <danielso89@gmail.com>
2020-06-17 23:37:12 -0700

    first commit: selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+/**
+ * selection_sort - a fucntion that sorts an array of integers in ascending
+ * order using selection sort algorithm
+ * @array: array of integers
+ * @size: number of elements in array
+ */
+void selection_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+
+	
+}
